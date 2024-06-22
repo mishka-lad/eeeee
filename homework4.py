d4d5c01385fd52d7160e6c505a2df73a6c9c7ff3
mishka-lad <mishka.marochkin.1996@mail.ru>
2024-06-22 16:03:48 +1000

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+my_string = input()
+my_string = len(my_string)
+print(my_string)
+
+
+my_string = input()
+print(my_string .upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
